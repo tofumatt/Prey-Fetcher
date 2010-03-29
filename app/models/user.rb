a61c0c748dd9183bbba07c5296fbc20bfe17aaed
Matthew Riley MacPherson <matt@lonelyvegan.com>
2010-03-29 18:07:38 -0300

    Remove mention checks
    User model only checks for DMs, as the Streaming API is now used for mention checks.

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,8 +22,7 @@ class User < ActiveRecord::Base
         :twitter_user_id => twitter_user.id,
         :twitter_username => twitter_user.screen_name,
         :access_key => session[:access_token][0],
-        :access_secret => session[:access_token][1],
-        :protected => twitter_user.protected
+        :access_secret => session[:access_token][1]
       )
       
       # Save the record, so now we can do lookups on this user
@@ -50,37 +49,37 @@ class User < ActiveRecord::Base
     end
   end
   
-  def process_response(tweet_type, prowl)
+  def process_dms(prowl)
     # Send any DM notifications -- handle exceptions from the JSON parser in case
     # Twitter sends us back malformed JSON or (more likely) HTML when it's over capacity
     begin
-      request, priority, since_id, sender = (tweet_type == "DM") ? [@dm_request, self.dm_priority, self.dm_since_id, 'sender'] : [@mention_request, self.mention_priority, self.mention_since_id, 'user']
+      request, priority, since_id = [@dm_request, self.dm_priority, self.dm_since_id]
       tweets = JSON.parse(request.response.body)
       if tweets.size > 0
         # The notification text depends on the number of new tweets
         if tweets.size == 1
-          event = "From @#{tweets.first[sender]['screen_name']}"
+          event = "From @#{tweets.first['sender']['screen_name']}"
           description = tweets.first['text']
         elsif tweets.size == 11
-          event = "Over 10 #{tweet_type}s! Latest from @#{tweets.first[sender]['screen_name']}"
+          event = "Over 10 DMs! Latest from @#{tweets.first['sender']['screen_name']}"
           description = tweets.first['text']
         else
-          event = "#{tweets.size} #{tweet_type}s. Latest from @#{tweets.first[sender]['screen_name']}"
+          event = "#{tweets.size} DMs. Latest from @#{tweets.first['sender']['screen_name']}"
           description = tweets.first['text']
         end
         
         # Update this users's since_id
-        update_attribute("#{tweet_type.downcase}_since_id", tweets.first['id'])
+        update_attribute('dm_since_id', tweets.first['id'])
         
         # A since_id of 1 means the user is brand new -- we don't send notifications on the first check
-        if since_id > 1
-          prowl.add(:application => APPNAME + ' ' + tweet_type, :apikey => self.prowl_api_key, :priority => priority, :event => event, :description => description)
+        if since_id != 1
+          prowl.add(:application => APPNAME + ' DM', :apikey => self.prowl_api_key, :priority => priority, :event => event, :description => description)
           Notification.new(:twitter_user_id => self.twitter_user_id).save
         end
       end
     rescue JSON::ParserError # Bad data (probably not even JSON) returned for this response
       logger.error Time.now.to_s + '   @' + self.twitter_username
-      logger.error 'Twitter was over capacity for @' + self.twitter_username + '? Couldn\'t make a usable array from JSON data.'
+      logger.error 'Twitter was over capacity for @' + self.twitter_username + "? Couldn't make a usable array from JSON data."
     rescue Exception # Bad data or some other weird response
       logger.error Time.now.to_s + '   @' + self.twitter_username
       logger.error 'Error getting data for @' + self.twitter_username + '. Twitter probably returned bad data.'
@@ -106,27 +105,8 @@ class User < ActiveRecord::Base
     @dm_request
   end
   
-  def check_mentions
-    uri = 'https://twitter.com/statuses/mentions.json'
-    params = {
-      'count' => 11,
-      'since_id' => self.mention_since_id,
-      'prey_fetcher_twitterid' => self.twitter_user_id
-    }
-    
-    @mention_request = Typhoeus::Request.new(uri,
-      :user_agent => USER_AGENT,
-      :method => :get,
-      :headers => { :Authorization => SOAuth.header(uri, oauth, params) },
-      :params => params
-    )
-    
-    # Return the request object (usually to Hydra)
-    @mention_request
-  end
-  
-  # Method called by cron to check all user accounts for new DMs and
-  # mentions, then send any notifications to Prowl
+  # Method called by cron to check all user accounts for new DMs,
+  # then send any notifications to Prowl
   def self.check_twitter
     require 'fastprowl'
     require 'soauth'
@@ -150,7 +130,7 @@ class User < ActiveRecord::Base
     # Loop through each user again, sending Prowl notifications if necessary
     users.each do |u|
       # Again, skip users with no key
-      u.process_response('DM', prowl) if u.enable_dms && !u.prowl_api_key.blank?
+      u.process_dms(prowl) if u.enable_dms && !u.prowl_api_key.blank?
     end
     
     # Send all the prowl notifications
