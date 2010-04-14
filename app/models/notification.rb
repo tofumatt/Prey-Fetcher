5c1a6a9b0fda7f9f1b8aa5ab10228033eeab0758
Matthew Riley MacPherson <matt@lonelyvegan.com>
2010-04-14 17:57:28 -0300

    Don't cache notification count
    Just do a count * on the DB.

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -4,12 +4,4 @@ class Notification < ActiveRecord::Base
 	def self.this_month
 	  count(:conditions => { :created_at => Date.today - 30..Date.tomorrow })
 	end
-	
-	# Return the total number of notifications sent
-	def self.count_all_from_cache
-	  @count = Rails.cache.fetch('Notification_count') { { :value => count, :expiry => (Time.now.to_i + 300) } }
-	  Rails.cache.delete('Notification_count') if @count[:expiry] < Time.now.to_i
-	  
-	  @count[:value]
-	end
 end
