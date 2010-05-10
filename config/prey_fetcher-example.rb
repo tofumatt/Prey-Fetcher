63d574f8a71b4beaa041f36fcea545937b13aea1
Matthew Riley MacPherson <matt@lonelyvegan.com>
2010-05-09 21:15:34 -0300

    Add example config files
    Layout of config files provided; actual site configs aren't stored in the git repo. Fill in your own if you wanted to run a clone.

---
+++ config/prey_fetcher-example.rb
@@ -0,0 +1,15 @@
+# Custom settings used by Prey Fetcher
+APPNAME = 'Prey Fetcher'
+MAX_CONCURRENCY = 50
+OAUTH_SETTINGS = {
+	'callback' => 'http://preyfetcher.com/verify',
+	'consumer_key' => '',
+	'consumer_secret' => ''
+}
+PROWL_PROVIDER_KEY = ''
+SESSION_SECRET = ''
+TWITTER_CREDENTIALS = {
+  :username => '',
+  :password => ''
+}
+USER_AGENT = 'Prey Fetcher 2.1 (http://preyfetcher.com)'
