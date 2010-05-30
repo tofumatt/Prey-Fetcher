8b0e1c593251442096b3769bd87c6f39a4e776d9
Matthew Riley MacPherson <matt@lonelyvegan.com>
2010-05-30 18:48:06 -0300

    Double JS animation speed
    Make the transitions faster, and add the list transition to the JS.

--- public/javascripts/jquery.preyfetcher.js
+++ public/javascripts/jquery.preyfetcher.js
@@ -1,20 +1,29 @@
 (function($) {
 	$().ready(function() {
-		var anim_speed = 500;
+		var ANIMATION_SPEED = 250;
 		
 		$('body').addClass('js');
 		
+		// DMs on the Settings page
 		$('#user_enable_dms').click(function(event) {
-			$('#dm-priority-container').slideToggle(anim_speed);
+			$('#dm-priority-container').slideToggle(ANIMATION_SPEED);
 		});
 		if ($('#user_enable_dms:checked').length == 1)
 			$('#dm-priority-container').show();
 		
+		// Mentions on the Settings page
 		$('#user_enable_mentions').click(function(event) {
-			$('#mention-priority-container').slideToggle(anim_speed);
+			$('#mention-priority-container').slideToggle(ANIMATION_SPEED);
 		});
 		if ($('#user_enable_mentions:checked').length == 1)
 			$('#mention-priority-container').show();
+		
+		// Lists on the Settings page
+		$('#user_enable_list').click(function(event) {
+			$('#list-priority-container').slideToggle(ANIMATION_SPEED);
+		});
+		if ($('#user_enable_list:checked').length == 1)
+			$('#list-priority-container').show();
 		
 	});
 })(jQuery);
