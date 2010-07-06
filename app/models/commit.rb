8b3b40ac6944bfa2d5bf333bd2b89a215e7287fc
Matthew Riley MacPherson <matt@lonelyvegan.com>
2010-07-06 01:31:19 -0300

    Use views folder

--- app/models/commit.rb
+++
@@ -1,4 +0,0 @@
-class Commit < ActiveRecord::Base
-  validates_uniqueness_of :sha
-  validates_presence_of :sha, :url, :author_name, :timestamp
-end
--- app/models/notification.rb
+++
@@ -1,7 +0,0 @@
-class Notification < ActiveRecord::Base
-  belongs_to :user, :foreign_key => :twitter_user_id
-	
-	def self.this_month
-	  count(:conditions => { :created_at => Date.today - 30..Date.tomorrow })
-	end
-end
