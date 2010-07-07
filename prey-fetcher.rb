c0a2e9d9ffea7816c75d43d0409e218f43eb98d1
Matthew Riley MacPherson <matt@lonelyvegan.com>
2010-07-07 20:26:48 -0300

    Display simple pages and errors

--- prey-fetcher.rb
+++ prey-fetcher.rb
@@ -14,6 +14,46 @@ set :root, File.dirname(__FILE__)
 set :public, "public"
 set :views, "views"
 
+configure do
+  # Load app config from YAML and set it in a constant
+  require "yaml"
+  
+  config = YAML.load_file("config.yml")
+  # Local config file for development, testing, overrides
+  unless Sinatra::Application.environment == 'production'
+    config.merge!(YAML.load_file("config_local.yml")) if File.exist?(File.join(File.dirname(__FILE__), "config_local.yml"))
+  end
+  
+  # Assemble some extra config values from those already set
+  config['app']['url'] = "http://#{config['app']['domain']}"
+  config['app']['user_agent'] = "#{config['app']['name']} #{config['app']['version']} (#{config['app']['url']})"
+  
+  # Put it in a constant so it's not tampered with
+  AppConfig = config
+end
+
+helpers do
+  def asset(file)
+    "http://#{AppConfig['app']['asset_domain']}/#{file}"
+  end
+  
+  # Return a number as a string with commas
+  def number_format(number)
+    (s=number.to_s;x=s.length;s).rjust(x+(3-(x%3))).scan(/.{3}/).join(',').strip.sub(/^,/, '')
+  end
+end
+
+# We'll need sessions
+use Rack::Session::Cookie, :key => '_preyfetcher_session',
+  :domain => AppConfig['app']['domain'],
+  :path => '/',
+  :expire_after => 2592000, # In seconds
+  :secret => AppConfig['app']['session_key']
+
+# Load the Twitter middleware
+use Twitter::Login, :consumer_key => AppConfig['twitter']['oauth']['consumer_key'], :secret => AppConfig['twitter']['oauth']['consumer_secret']
+helpers Twitter::Login::Helpers
+
 class Notification
   include DataMapper::Resource
   
@@ -78,6 +118,46 @@ class User
   end
 end
 
+# Index action -- show the homepage
 get "/" do
-  "Push your tweets to nowhere."
+  @title = "Open Source Twitter Push Notifications"
+  erb :index
+end
+
+# Show the FAQ
+get "/faq" do
+  @title = "Questions About Prey Fetcher"
+  erb :faq
+end
+
+# Show the Privacy jazz
+get "/privacy" do
+  @title = "Promise To Not Be Evil"
+  erb :privacy
+end
+
+# Show the OSS page
+get "/open-source" do
+  @title = "Open Source"
+  erb :open_source
+end
+
+# 404
+not_found do
+  @title = "Page Not Found"
+  erb :'404'
+end
+
+# Any other error
+error do
+  @title = "Bad Server, Bad!"
+  erb :'500'
+end
+
+# Non production mode routes
+unless Sinatra::Application.environment == 'production'
+  get '/stylesheets/:file.css' do |file|
+    content_type 'text/css', :charset => 'utf-8'
+    sass file.to_sym
+  end
 end
