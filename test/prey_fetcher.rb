568a8c7a682dfbcad25eb39b3f45493167483fcd
Matthew Riley MacPherson <matt@lonelyvegan.com>
2010-09-29 22:28:25 -0300

    Add really simple tests

---
+++ test/prey_fetcher.rb
@@ -0,0 +1,32 @@
+require File.join(File.dirname(__FILE__), '../', "prey_fetcher.rb")
+require 'test/unit'
+require 'rack/test'
+
+ENV['RACK_ENV'] = 'test'
+
+class PreyFetcherTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+  
+  def app
+    Sinatra::Application
+  end
+  
+  def test_homepage_works_as_anonymous
+    get '/'
+    assert last_response.ok?
+  end
+  
+  def test_account_fails_as_anonymous
+    get '/account'
+    assert !last_response.ok?
+    put '/account'
+    assert !last_response.ok?
+    delete '/account'
+    assert !last_response.ok?
+  end
+  
+  def test_logout_fails_as_anonymous
+    get '/logout'
+    assert !last_response.ok?
+  end
+end
