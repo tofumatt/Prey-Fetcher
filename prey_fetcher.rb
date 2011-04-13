8345edb800bd60c4a74f43cd9ba84f80aa17c459
Matthew Riley MacPherson <matt@lonelyvegan.com>
2011-04-13 01:06:53 -0300

    Bump version number

--- prey_fetcher.rb
+++ prey_fetcher.rb
@@ -21,7 +21,7 @@ module PreyFetcher
   
   # Current version number + prefix. Gets used in
   # as the User Agent in REST/Streaming requests.
-  VERSION = "5.0"
+  VERSION = "5.0.1"
   
   # Setup Prey Fetcher config and such
   def self.boot!
